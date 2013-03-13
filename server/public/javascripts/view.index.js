721305430d9fe10e81eb53227a52343f987cbfb1
Jiang Youxin <jiangyouxin9@gmail.com>
2013-03-13 15:07:34 +0100

    test WebView.addJavascirptInterface success.

--- server/public/javascripts/view.index.js
+++ server/public/javascripts/view.index.js
@@ -1,24 +1,8 @@
 $(function() {
   $('#right-column').hide();
 
-  $('li.hero-name a').click(function() {
-  
-    var name = $(this).text();
-    
-    $('#right-column h2').text(name);
-  
-    $('#facts li').remove();
-    
-    $.getJSON('/hero/' + name, function(data) {
-      for (var i = 0; i < data.length; i++) {
-        $('<li>').appendTo('#facts').text(data[i]);
-      }
-    });
-    
-    $('#right-column').show();
-    
-    return false;
-  });
+  var imei = Phone.getImei();
+  $('#refresh').text(imei);
   
   $('#add-new-fact').click(function() {
     
