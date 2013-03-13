f330a8e17c35bf4db0eaa015c93abb79fa9125de
Jiang Youxin <jiangyouxin9@gmail.com>
2013-03-13 15:47:26 +0800

    test json output

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,3 +6,11 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.update = function(req, res){
+  var obj = new Object();
+  obj.x = "Hello World";
+  obj.y = "Hello Sb";
+  var json = JSON.stringify(obj);
+  res.send(json);
+}
